8f2705fb046505d4ee6c481c2a67d03fbf68d47c
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-29 19:19:15 +0530

    added to DataTypes

---
+++ src/main/java/com/banking/bankapplication/core/DataTypes.java
@@ -0,0 +1,33 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class DataTypes {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		String name="ChenchuRaja";
+		int age=23;
+		double money=300;
+		float f=76f;
+		long aadharNum=711869076371l;
+		boolean b=true;
+		char alphabet='A';
+		
+      System.out.println("Name is:--"+name);
+      System.out.println("Age is:--"+age);
+      System.out.println("Aadhar Num is:--"+aadharNum);
+      System.out.println("Alphabet is:--"+alphabet);
+      System.out.println("Money is:--"+money);
+      System.out.println("Float Value is:--"+f);
+      System.out.println("Boolean Value is:--"+b);
+	}
+
+}
