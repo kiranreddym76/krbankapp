f39ae6ced5da38d50d8f30455e0297dd58c09087
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-29 17:48:35 +0530

    added EligibulForVoteProg

---
+++ src/main/java/com/banking/bankapplication/core/EligibulForVote.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class EligibulForVote {
+
+	public static void main(String[] args) {
+		
+		int age=18;
+		if(age==18) {
+			System.out.println(age+":Is EligibulForVotting");
+		} else {
+			System.out.println(age+":Is Not-EligibulForVotting");
+		}
+	}
+}
