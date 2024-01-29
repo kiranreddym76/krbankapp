5f9e43bd86fef982ac421effc5bd641ec117e7da
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-29 17:24:45 +0530

    added sumofnum

---
+++ src/main/java/com/banking/bankapplication/core/SumOfNum.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class SumOfNum {
+
+	public static void main(String[] args) {
+		
+		int sum=0;
+		
+		for(int firstNum=1;firstNum<=5;firstNum++) {
+			
+			sum=sum+firstNum;
+		
+		}
+		System.out.println("Sum OF 5Value Is:"+sum);
+	}
+}
