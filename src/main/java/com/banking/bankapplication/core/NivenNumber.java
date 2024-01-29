5a745aefb2196a39844ed2a8eaf8cdad8e6b1b51
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-29 18:55:43 +0530

    Merge branch 'feature/chenchuraja' of https://github.com/kiranreddym76/krbankapp.git into feature/chenchuraja

---
+++ src/main/java/com/banking/bankapplication/core/NivenNumber.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class NivenNumber {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+
+		   Scanner scn=new Scanner(System.in);
+		   System.out.println("Enter The Number:---");
+		   int num=scn.nextInt();
+		
+		 if(num<0) {
+			   System.out.println("No-Negatives");
+		   } else {
+			   
+			   int sum=0;
+			  int temp=num;
+			  
+			   while(num>0) {
+				   int rem=num%10;
+				   sum=sum+rem;
+				   num=num/10;
+			   }
+			   if(temp%sum==0) {
+				   System.out.println("Niven Number");
+			   } else {
+				   System.out.println("No-Niven Number");
+			   }
+
+	}
+
+}
+}
