56730efcdd0feb1937d8e56551556b56d3c6efaa
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-30 08:41:19 +0530

    added to DataTypes

--- src/main/java/com/banking/bankapplication/core/DataTypes.java
+++ src/main/java/com/banking/bankapplication/core/DataTypes.java
@@ -15,19 +15,18 @@ public class DataTypes {
 		
 		String name="ChenchuRaja";
 		int age=23;
-		double money=300;
-		float f=76f;
 		long aadharNum=711869076371l;
-		boolean b=true;
+		double money=3000;
+		float f=89f;
 		char alphabet='A';
 		
-      System.out.println("Name is:--"+name);
-      System.out.println("Age is:--"+age);
-      System.out.println("Aadhar Num is:--"+aadharNum);
-      System.out.println("Alphabet is:--"+alphabet);
-      System.out.println("Money is:--"+money);
-      System.out.println("Float Value is:--"+f);
-      System.out.println("Boolean Value is:--"+b);
+		System.out.println("Name is:--"+name);
+		System.out.println("Age is:--"+age);
+		System.out.println("AadharNum is:--"+aadharNum);
+		System.out.println("Money is:--"+money);
+		System.out.println("Float Value is:--"+f);
+		System.out.println("Alphabet is:--"+alphabet);
+
 	}
 
 }
