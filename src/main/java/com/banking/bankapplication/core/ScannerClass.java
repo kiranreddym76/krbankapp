de7e64c881b7adff13459434d27ce6a0812110f3
harit <harit@Haritha>
2024-01-30 12:24:23 +0530

    added to scannerclass

---
+++ src/main/java/com/banking/bankapplication/core/ScannerClass.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+import java.util.Scanner;
+
+/**
+ * 
+ */
+public class ScannerClass {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scn=new Scanner(System.in);
+		System.out.println("Enter the two values");
+		int FirstEle=scn.nextInt();
+		int SecondEle=scn.nextInt();
+		int sum=FirstEle+SecondEle;
+
+	}
+
+}
