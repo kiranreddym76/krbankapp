5bbfc1c41262e5a40fe5cb7896395cde34cf5d7d
harit <harit@Haritha>
2024-01-30 12:05:09 +0530

    added to DataTypes

---
+++ src/main/java/com/banking/bankapplication/core/DataTypes.java
@@ -0,0 +1,16 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class DataTypes {
+
+	public static void main(String[] args) {
+		
+		int number=98;
+		System.out.println(number);
+	}
+}
