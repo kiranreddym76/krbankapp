65e17efb7546eed5d83e3f1e2fd252ff5605eee6
Bindu <Bindu@DESKTOP-DKJEIH7>
2024-01-30 11:58:43 +0530

    added to IfExample

---
+++ src/main/java/com/banking/bankapplication/core/IfExample.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class IfExample {
+
+	public static void main(String[] args) {
+		
+		int num=23;
+		if(num>25) {
+			System.out.println("Good Morning");
+		}
+	}
+}
