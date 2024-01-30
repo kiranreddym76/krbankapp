2f1d09f0252b1ececb0d101a79a1af95b2bcdb23
ChenchuRaja1502 <chenchuraja143@gmail.com>
2024-01-30 10:25:59 +0530

    added to ifexampule program

---
+++ src/main/java/com/banking/bankapplication/core/controlstatments/IfExampules.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core.controlstatments;
+
+/**
+ * 
+ */
+public class IfExampules {
+
+	public static void main(String[] args) {
+		
+		if(10>20) {
+			System.out.println("Exampule");
+		}
+	}
+}
