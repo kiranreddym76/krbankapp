1c67e9a4b726f9a8cccb269998d18e46ffa2d064
harit <harit@Haritha>
2024-01-31 11:24:10 +0530

    added to multiplication

---
+++ src/main/java/com/banking/bankapplication/core/Multiplication.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class Multiplication {
+
+	public static void main(String[] args) {
+		
+		int num=5;
+		for(int firstNum=1;firstNum<=10;firstNum++) {
+			System.out.println(num +"*" +firstNum + "=" +(num*firstNum) );
+		}
+			
+			
+	}
+}
