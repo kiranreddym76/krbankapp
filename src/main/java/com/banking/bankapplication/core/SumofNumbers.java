e40ce31e4bef219119253b98740101f458e0e13c
harit <harit@Haritha>
2024-01-31 09:46:47 +0530

    added to data types

---
+++ src/main/java/com/banking/bankapplication/core/SumofNumbers.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class SumofNumbers {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int firstEle=19980202;
+		int secondEle=20020807;
+		int Sum=firstEle+secondEle;
+		System.out.println(Sum);
+	}
+
+}
