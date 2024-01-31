aac3712d5ea9201026a27e5af9a123cf55bb1a03
harit <harit@Haritha>
2024-01-31 10:22:16 +0530

    added to demo

---
+++ src/main/java/com/banking/bankapplication/core/Demo.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class Demo {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int y=7;
+		for(int i=1;i<=y;i++) {
+			System.out.println(i);	}
+	}
+
+}
