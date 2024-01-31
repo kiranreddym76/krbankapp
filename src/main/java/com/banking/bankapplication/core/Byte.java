440788594903bc814aec15c87c7a60af1da3a430
harit <harit@Haritha>
2024-01-31 19:17:24 +0530

    added to byte

---
+++ src/main/java/com/banking/bankapplication/core/Byte.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.banking.bankapplication.core;
+
+/**
+ * 
+ */
+public class Byte {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte c=2;
+		byte h=7;
+		byte L=(byte)(c+h);{
+			System.out.println(L);
+		}
+
+	}
+
+}
